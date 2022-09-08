091394891231934618e282d3d9c37cf60e744c99
Elias Grey <eliasgrey9@gmail.com>
2022-09-08 11:39:38 -0700

    2ndCommit

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 const body = document.querySelector("body");
-const inputLink = document.querySelector(".link")
-const inputName = document.querySelector(".name")
-const btn = document.querySelector("button")
+const inputLink = document.querySelector(".link");
+const inputName = document.querySelector(".name");
+const btn = document.querySelector("button");
 
 //Creates ul to place li elements in
-const ul = document.createElement("ul");
+let ul = document.createElement("ul");
 body.appendChild(ul);
 
 //Creates li element to appear in ul and be appended by the linkAndName const.
-const li = document.createElement("li");
+let li = document.createElement("li");
+ul.appendChild(li);
 
 let listArray = [];
 let linkAndName = "";
 
 btn.addEventListener("click", (event) => {
-linkAndName = inputLink.value + " " + inputName.value;
-listArray.push(linkAndName);
-console.log(listArray);
-}
-);
-
+  linkAndName = inputLink.value + " " + inputName.value;
+  listArray.push(linkAndName);
+  console.log(listArray);
+});
 
 //renders each item in an array as a list item
- const render = (myList) =>{
- myList.forEach((token)=>{li.append(token);
+const render = (token) => {
+token.forEach((arrItem) =>{
+ul.push(arrItem);
+return ul;
 })
- ul.appendChild(li);
- return ul;
-}
+};
+render(listArray);
 
-//executes render function
-console.log(render(listArray));
