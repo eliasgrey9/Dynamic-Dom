4a986cc2f9a1be99485016bff3795803ab864c3b
Elias Grey <eliasgrey9@gmail.com>
2022-09-08 11:57:49 -0700

    3rdPush

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const body = document.querySelector("body");
+const container = document.querySelector(".container");
 const inputLink = document.querySelector(".link");
 const inputName = document.querySelector(".name");
 const btn = document.querySelector("button");
 
 //Creates ul to place li elements in
 let ul = document.createElement("ul");
-body.appendChild(ul);
+container.appendChild(ul);
 
 //Creates li element to appear in ul and be appended by the linkAndName const.
 let li = document.createElement("li");
@@ -17,15 +18,18 @@ let linkAndName = "";
 btn.addEventListener("click", (event) => {
   linkAndName = inputLink.value + " " + inputName.value;
   listArray.push(linkAndName);
+  render(listArray);
   console.log(listArray);
 });
 
 //renders each item in an array as a list item
 const render = (token) => {
 token.forEach((arrItem) =>{
-ul.push(arrItem);
-return ul;
+const list = document.createElement("li");
+list.textContent = arrItem;
+ul.append(list);
 })
 };
 render(listArray);
 
+console.log(ul);
